b24f170a6353dd8ba464980e74857ed5f7de6c0d
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-10-02 13:32:28 -0400

    Add markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,64 @@
+import memoryIcon from './assets/images/icon-memory.svg';
+import reactionIcon from './assets/images/icon-reaction.svg';
+import verbalIcon from './assets/images/icon-verbal.svg';
+import visualIcon from './assets/images/icon-Visual.svg';
+
 function App() {
-  return <h1>Hello world!</h1>;
+  return (
+    <article>
+      <section>
+        <h1>Your Result</h1>
+        <div>
+          <span>76</span>
+          <span>of 100</span>
+        </div>
+        <p>Great</p>
+        <p>
+          You scored higher than 65% of the people who have taken these tests.
+        </p>
+      </section>
+      <section>
+        <h2>Summary</h2>
+        <div>
+          <img src={memoryIcon} />
+          <p>Visual</p>
+          <p>
+            <span>72</span>
+            <span>/</span>
+            <span>100</span>
+          </p>
+        </div>
+        <div>
+          <img src={reactionIcon} />
+          <p>Visual</p>
+          <p>
+            <span>72</span>
+            <span>/</span>
+            <span>100</span>
+          </p>
+        </div>
+        <div>
+          <img src={verbalIcon} />
+          <p>Visual</p>
+          <p>
+            <span>72</span>
+            <span>/</span>
+            <span>100</span>
+          </p>
+        </div>
+        <div>
+          <img src={visualIcon} />
+          <p>Visual</p>
+          <p>
+            <span>72</span>
+            <span>/</span>
+            <span>100</span>
+          </p>
+        </div>
+        <button>Continue</button>
+      </section>
+    </article>
+  );
 }
 
 export default App;
